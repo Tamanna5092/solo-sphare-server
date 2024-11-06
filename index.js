9b2b46c6a7f3b79842c4777e2e6c6a65c0411b28
Tamanna@5092 <tamannaaktermoni51@gmailcom>
2024-11-06 22:37:52 +0600

    commit

--- index.js
+++ index.js
@@ -1,16 +1,57 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
-const port = process.env.PORT || 9000
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 9000;
 
-const app = express()
+const app = express();
 
 const corsOperation = {
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
-    credential: true,
-    optionSuccessStatus: 200
-}
-app.use(cors(corsOperation))
-app.use(express.json())
+  origin: ["http://localhost:5173", "http://localhost:5174"],
+  credential: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOperation));
+app.use(express.json());
 
-app.listen(port, () => console.log(`Server running on port ${port}`))
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.abrfq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    
+    const jobCollection = client.db('SoloSphare').collection('jobs')
+    const bidsCollection = client.db('SoloSphare').collection('bids')
+
+
+app.get("/jobs", async (req, res) => {
+  const result = await jobCollection.find().toArray()
+  res.send(result)
+});
+
+
+
+    // Connect the client to the server	(optional starting in v4.7)
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello Solo Sphare server");
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
